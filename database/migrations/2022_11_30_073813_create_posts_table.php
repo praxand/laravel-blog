1449da16448285206217620c7d8387ef93b59d38
Jay <jay.cheung2005@gmail.com>
2022-11-30 09:56:43 +0100

    database migrations

---
+++ database/migrations/2022_11_30_073813_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('title')
+                ->unique();
+            $table->string('slug')
+                ->unique();
+            +$table->text('excerpt');
+            $table->longtext('body');
+            $table->string('image_path')
+                ->nullable();
+            $table->enum('status');
+            $table->timestamp('published_at')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
