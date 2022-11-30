3d89a9ae6155fba7095cb110918cd258d88906f2
Jay <jay.cheung2005@gmail.com>
2022-11-30 14:06:09 +0100

    updated all models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
