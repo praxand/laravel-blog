18bd7c3bab5b038da475b6efdffbc8f0e4fb9a09
Jay <jay.cheung2005@gmail.com>
2022-11-30 11:12:22 +0100

    added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +19,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->create()->each(function ($user) {
+            Post::factory()->create([
+                'user_id' => $user->id,
+            ])->each(function ($post) {
+                Category::factory(3)->create([
+                    'post_id' => $post->id,
+                ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+                Comment::factory(3)->create([
+                    'post_id' => $post->id,
+                    'user_id' => $post->user_id,
+                ]);
+
+                Like::factory(2)->create([
+                    'post_id' => $post->id,
+                    'user_id' => $post->user_id,
+                ]);
+            });
+        });
     }
 }
