b33c9eb87055d29197af0b86e855945a3da2e5c1
Jay <jay.cheung2005@gmail.com>
2022-12-01 10:32:05 +0100

    updated styling

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,23 @@
 <x-layout>
+    <div class="container mx-auto px-5 lg:max-w-screen-sm">
+        @foreach ($posts as $post)
+        @if ($post->status == 'published')
+        <a class="block border w-full mb-10 p-5 rounded" href="{{ $post->slug }}">
+            <img src="{{ $post->image_path }}" alt="{{ $post->title }}" class="w-full h-64 object-cover">
 
+            <div class="flex flex-col justify-between flex-1">
+                <h2 class="my-6 text-xl font-semibold">{{ $post->title }}</h2>
+                <p class="mb-6 leading-loose">{{ $post->excerpt }}</p>
+
+                <div class="flex items-center text-sm">
+                    <img src="{{ $post->user->image_path }}" alt="{{ $post->user->name }}"
+                        class="w-10 h-10 rounded-full">
+                    <span class="ml-2">{{ $post->user->name }}</span>
+                    <span class="ml-auto">{{ $post->published_at->toFormattedDateString() }}</span>
+                </div>
+            </div>
+        </a>
+        @endif
+        @endforeach
+    </div>
 </x-layout>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,58 @@
 <x-layout>
+    <div class="container mx-auto px-5 lg:max-w-screen-sm mt-20">
+        <h1 class="mb-5 text-2xl font-semibold">{{ $post->title }}</h1>
 
+        <div class="flex items-center text-sm">
+            <span>{{ $post->published_at->toFormattedDateString() }}</span>
+            <p>&nbsp; — &nbsp;</p>
+            @foreach ($post->categories as $category)
+            {{-- {{ route('posts.slugs', $category->slug) }} --}}
+            <a href="#" class="mr-1 hover:underline">#{{ $category->name }}</a>
+            @endforeach
+        </div>
+
+        <div class="mt-5 leading-loose flex flex-col justify-center items-center">
+            <p><img src="{{ $post->image_path }}" alt="{{ $post->title }}"></p>
+            <p class="my-3 font-semibold">{{ $post->excerpt }}</p>
+            <p>{{ $post->body }}</p>
+        </div>
+
+        <div class="mt-10 lg:flex items-center p-5 border rounded">
+            <div class="w-full lg:w-1/6 text-center lg:text-left">
+                <img src="{{ $post->user->image_path }}" alt="{{ $post->user->name }}"
+                    class="rounded-full w-32 lg:w-full">
+            </div>
+            <div class="lg:pl-5 leading-loose text-center lg:text-left w-full lg:w-5/6">
+
+                <div class="flex items-center text-sm">
+                    <div>
+                        By <span class="font-bold">{{ $post->user->name }}</span>
+                        <div class="text-sm">
+                            <p>{{ $post->likes->count() }} Likes</p>
+                        </div>
+                    </div>
+
+                    <span class="ml-auto">
+                        <a href="#">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Like
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="border-t my-8">
+        <div class="container mx-auto px-5 lg:max-w-screen-sm mt-14">
+            <p>{{$post->comments->count()}} Comments</p>
+            @foreach ($post->comments as $comment)
+            <div class="my-3">
+                <p class="text-md font-semibold">{{ $comment->user->name }}</p>
+                <p>{{ $comment->body }}</p>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
