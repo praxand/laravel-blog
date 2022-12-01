311809b77c37121405097752f7b619a890220fff
Jay <jay.cheung2005@gmail.com>
2022-12-01 10:59:17 +0100

    added feed page

---
+++ resources/views/feed/index.blade.php
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+
+<rss version="2.0">
+    <channel>
+        <title>Laravel Blog</title>
+        <link>https://127.0.0.1:8000</link>
+
+        @foreach ($posts as $post)
+        @if ($post->status == 'published')
+        <item>
+            <title>
+                <![CDATA[ {{ $post->title }} ]]>
+            </title>
+
+            <link rel="alternate" href="{{ route('posts.show', $post->slug) }}" />
+
+            <id>{{ route('posts.show', $post->slug) }}</id>
+
+            <author>
+                <name>{{ $post->user->name }}</name>
+            </author>
+
+            <summary type="html">
+                <![CDATA[ {{ $post->excerpt }} ]]>
+            </summary>
+
+            <updated>{{ $post->updated_at->toRssString() }}</updated>
+        </item>
+        @endif
+        @endforeach
+    </channel>
+</rss>
