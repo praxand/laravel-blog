e46234b5fb16836c80ec24d03960b8ec11e0c312
Jay <jay.cheung2005@gmail.com>
2022-12-02 10:45:28 +0100

    dashboard only available for admins

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('feed', [RSSFeedController::class, 'index'])->name('feed.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
