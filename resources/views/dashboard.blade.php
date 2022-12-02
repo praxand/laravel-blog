607d9717c6ae29759a47461fb368e0a3a40e9151
Jay <jay.cheung2005@gmail.com>
2022-12-02 11:37:00 +0100

    temporarily layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12 px-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg h-48">
                     <div class="p-6 text-gray-900">
                         Registered Users: {{ $users->count() }}
                         <p>
@@ -13,19 +13,19 @@
                     </div>
                 </div>
 
-                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg h-48">
                     <div class="p-6 text-gray-900">
                         Categories: {{ $categories->count() }}
                     </div>
                 </div>
 
-                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg h-48">
                     <div class="p-6 text-gray-900">
                         Comments: {{ $comments->count() }}
                     </div>
                 </div>
 
-                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg h-48">
                     <div class="p-6 text-gray-900">
                         Posts: {{ $posts->count() }}
                     </div>
