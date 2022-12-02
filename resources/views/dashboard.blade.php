58d9906f4215ff0911c678165814743e23b06291
Jay <jay.cheung2005@gmail.com>
2022-12-02 11:32:25 +0100

    temporarily dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,34 @@
 <x-app-layout>
     <div class="py-12 px-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Registered Users: {{ $users->count() }}
+                        <p>
+                            @foreach ($users as $user)
+                            {{ $user->name }} {{ $user->admin ? '(Admin)' : '' }}@if(!$loop->last),@endif
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Categories: {{ $categories->count() }}
+                    </div>
+                </div>
+
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Comments: {{ $comments->count() }}
+                    </div>
+                </div>
+
+                <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        Posts: {{ $posts->count() }}
+                    </div>
                 </div>
             </div>
         </div>
