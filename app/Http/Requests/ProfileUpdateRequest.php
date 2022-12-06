5b4ababbf3e6d93a62b41dcf2ba7faced0eaa59d
Jay <jay.cheung2005@gmail.com>
2022-12-06 12:37:52 +0100

    added image validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
+            'image_path' => ['image'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
