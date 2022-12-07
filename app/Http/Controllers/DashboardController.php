5addd5c79c32409088ecd57da8de1e5e600a65d9
Jay <jay.cheung2005@gmail.com>
2022-12-07 13:42:47 +0100

    removed use of request

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
