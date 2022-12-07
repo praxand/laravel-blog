be6b3295c64e9e429c48578365284fda939c1a72
Jay <jay.cheung2005@gmail.com>
2022-12-07 10:53:36 +0100

    added mimes

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['string', 'max:255'],
-            'image_path' => ['image'],
+            'image_path' => ['image', 'mimes:png,jpg,jpeg,jfif,pjpeg,pjp'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
