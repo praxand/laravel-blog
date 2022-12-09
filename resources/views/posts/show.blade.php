fcd74eff4a99b84af80bf6629f8d6db2eac4464c
Jay <jay.cheung2005@gmail.com>
2022-12-09 10:27:36 +0100

    added edit button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,6 +18,16 @@
                     </button>
                 </a>
 
+                @if (Auth::user()->id === $post->user->id || Auth::user()->admin)
+                <form action="{{ route('posts.edit', $post->slug) }}" method="get">
+                    @csrf
+
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Edit
+                    </button>
+                </form>
+                @endif
+
                 @if (Auth::user()->admin)
                 <form action="{{ route('posts.delete', $post->id) }}" method="post">
                     @csrf
