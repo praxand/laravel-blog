9e54e478231e8ab6ed86018cd80a50d0c718c093
Jay <jay.cheung2005@gmail.com>
2022-12-09 10:30:10 +0100

    added edit page

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,3 +1,9 @@
-<x-layout>
-
-</x-layout>
+<x-app-layout>
+    <div class="py-12 px-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                @include('posts.partials.update-form')
+            </div>
+        </div>
+    </div>
+</x-app-layout>
