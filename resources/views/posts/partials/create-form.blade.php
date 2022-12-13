edaab7616af254d7c1baf11bafb7e904dcd4b0f5
Jay <jay.cheung2005@gmail.com>
2022-12-13 09:51:15 +0100

    removed unnecessary code

--- resources/views/posts/partials/create-form.blade.php
+++ resources/views/posts/partials/create-form.blade.php
@@ -10,31 +10,25 @@
 
         <div>
             <x-input-label for="title" :value="__('Title')" />
-            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" :value="old('title')" required
-                autocomplete="title" />
+            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" required autocomplete="title" />
             <x-input-error class="mt-2" :messages="$errors->get('title')" />
         </div>
 
         <div>
             <x-input-label for="slug" :value="__('Slug')" />
-            <x-text-input id="slug" name="slug" type="text" class="mt-1 block w-full" :value="old('slug')" required
-                autocomplete="slug" />
+            <x-text-input id="slug" name="slug" type="text" class="mt-1 block w-full" required autocomplete="slug" />
             <x-input-error class="mt-2" :messages="$errors->get('slug')" />
         </div>
 
         <div>
             <x-input-label for="excerpt" :value="__('Excerpt')" />
-            <x-textarea-input id="excerpt" name="excerpt" cols="30" rows="5" class="mt-1 block w-full" required>
-                {{ old('excerpt') }}
-            </x-textarea-input>
+            <x-textarea-input id="excerpt" name="excerpt" cols="30" rows="5" class="mt-1 block w-full" required />
             <x-input-error class="mt-2" :messages="$errors->get('excerpt')" />
         </div>
 
         <div>
             <x-input-label for="body" :value="__('Body')" />
-            <x-textarea-input id="body" name="body" cols="30" rows="10" class="mt-1 block w-full" required>
-                {{ old('body') }}
-            </x-textarea-input>
+            <x-textarea-input id="body" name="body" cols="30" rows="10" class="mt-1 block w-full" required />
             <x-input-error class="mt-2" :messages="$errors->get('body')" />
         </div>
 
@@ -47,13 +41,13 @@
         <div>
             <x-input-label for="status" :value="__('Status')" />
             <x-dropdown-input id="status" name="status" class="mt-1 block w-full" required>
-                <option value="draft" {{ old('status') === 'draft' ? 'selected' : '' }}>
+                <option value="draft">
                     {{ __('Draft') }}
                 </option>
-                <option value="published" {{ old('status') === 'published' ? 'selected' : '' }}>
+                <option value="published">
                     {{ __('Published') }}
                 </option>
-                <option value="deleted" {{ old('status') === 'deleted' ? 'selected' : '' }}>
+                <option value="deleted">
                     {{ __('Deleted') }}
                 </option>
             </x-dropdown-input>
