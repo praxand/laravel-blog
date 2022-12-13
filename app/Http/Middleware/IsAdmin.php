c221d7801ab465301375ff2d9750c84a9ed3c3a7
Jay <jay.cheung2005@gmail.com>
2022-12-13 11:47:20 +0100

    fixed issue

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class IsAdmin {
 
@@ -24,6 +25,6 @@ class IsAdmin {
             return $next($request);
         }
 
-        return abort(404);
+        return Redirect::route('posts.index');
     }
 }
